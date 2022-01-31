0d514acb4c9a61f8d128532d8c8ec3889a0ce4f6
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-01-31 18:09:08 +0530

    Update tshirts.c

--- tshirts.c
+++ tshirts.c
@@ -1,24 +1,40 @@
 #include <stdio.h>
 #include <assert.h>
 
-char size(int cms) {
+char Shirtsize(int cms) {
     char sizeName = '\0';
-    if(cms < 38) {
+    if(cms >= 36 && cms < 38) 
+    {
         sizeName = 'S';
-    } else if(cms >= 38 && cms <= 42) {
+    } 
+    else if(cms >= 38 && cms <= 42) 
+    {
         sizeName = 'M';
-    } else if(cms > 42) {
+    } 
+    else if(cms > 42 && cms<=44) 
+    {
         sizeName = 'L';
+    }
+    else
+    {
+        sizeName = '\0';
     }
     return sizeName;
 }
+VerifyShirtSize(int size, char sizename)
+{
+    assert(Shirtsize(size) == sizename);
+}
 
-int main() {
-    assert(size(37) == 'S');
-    assert(size(40) == 'M');
-    assert(size(43) == 'L');
-    assert(size(38) == 'M');
-    assert(size(42) == 'M');
+int main()
+{
+    VerifyShirtSize(35, '\0');
+    VerifyShirtSize(37, 'S');
+    VerifyShirtSize(40, 'M');
+    VerifyShirtSize(43, 'L');
+    VerifyShirtSize(38, 'M');
+    VerifyShirtSize(42, 'M');
+    VerifyShirtSize(45, '\0');
     printf("All is well (maybe!)\n");
     return 0;
 }
