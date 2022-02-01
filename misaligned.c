bdbedc06d4b0f94b7bbdb49575aeabb2bdb65922
TharaniDevendran <91050185+TharaniDevendran@users.noreply.github.com>
2022-02-01 19:13:53 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -10,45 +10,48 @@ int numberOfMinorColors = sizeof(majorColor) / sizeof(majorColor[0]);
 typedef struct {
     const char* majorColor;
     const char* minorColor;
+    int pairNumber;
 } ColorPair;
 
-ColorPair GetColorFromPairNumber(int pairNumber) {
+ColorPair GetColorFromPairNumber(int PairNumber) {
     ColorPair colorPair;
-    colorPair.majorColor = majorColor[(pairNumber / numberOfMinorColors)];
-    colorPair.minorColor = minorColor[(pairNumber % numberOfMinorColors)];
+    colorPair.majorColor = majorColor[(PairNumber / numberOfMinorColors)];
+    colorPair.minorColor = minorColor[(PairNumber % numberOfMinorColors)];
+    colorPair.pairnumber = PairNumber;
     return colorPair;
 }
 
-void VerifyNumberToPair(int pairNumber, const char* expectedMajor, const char* expectedMinor){
+void VerifyColorMapForPairNumber(int pairNumber)
+{
     ColorPair colorPair = GetColorFromPairNumber(pairNumber);
     assert(colorPair.majorColor == expectedMajor);
     assert(colorPair.minorColor == expectedMinor);
 }
 
-void formatColorPrinter(int pairNumber, const char* MajorColor, const char* MinorColor)
+void formatColorPrinter(Colorpair colorpair)
 {
-    printf("%d | %s | %s\n", pairNumber, MajorColor, MinorColor);
+    printf("%d | %s | %s\n", colorpair.pairNumber, colorpair.majorColor, colorpair.minorColor);
 }
-int printColorMap() 
+int printColorMap(*ToformatColorPrinter(Colorpair))
 {
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) 
     {
         for(j = 0; j < 5; j++) {
-            formatColorPrinter(i * 5 + j, majorColor[i], minorColor[j]);
+            ToformatColorPrinter(GetColorFromPairNumber (i * 5 + j));
         }
     }
     return i * j;
 }
 
 int main() {
-    int result = printColorMap();
+    int result = printColorMap(&formatColorPrinter);
     assert(result == 25);
     for(int i = 0; i < 5; i++) 
     {
         for(int j = 0; j < 5; j++) 
         {
-                       VerifyNumberToPair( i * 5 + j, majorColor[i], minorColor[j]);
+                       VerifyColorMapForPairNumber( i * 5 + j);
         }
     }
     printf("All is well (maybe!)\n");
